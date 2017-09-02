ff5a5300b23e8176a236b809b1e89454ea8320eb
Ziggypop <zimhen7@gmail.com>
2017-09-02 13:15:25 -0400

    First pass at handling external files

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,21 @@
 #![macro_use]
 extern crate nom;
 extern crate test;
+extern crate clap;
+
+use clap::{Arg, App};
+use nom::IResult;
 
 use test::Bencher;
 
 use std::boxed::Box;
 use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::OpenOptions;
 
 mod datatype;
 mod lang_result;
@@ -26,7 +36,42 @@ use parser::{function, program};
 
 
 fn main() {
-    repl()
+
+    let matches = App::new("Toy Language")
+        .version("0.1.0")
+        .author("Henry Zimmerman")
+        .about("A toy language I made in a couple of days without thinking about it too much")
+        .arg(
+            Arg::with_name("file")
+            .value_name("File")
+            .help("The file that you want to interpret")
+            .takes_value(true)
+        )
+        .get_matches();
+
+
+    match matches.value_of("file") {
+        Some(filename) => {
+            let mut file_contents: String = String::new();
+            let file: File = OpenOptions::new().read(true).open(&filename).unwrap();
+            let mut buf_reader = BufReader::new(&file);
+            match buf_reader.read_to_string(&mut file_contents) {
+                Ok(_) => {},
+                Err(e) => eprintln!("Couldn't read the file {} because {}", filename, e)
+            }
+
+            match program(file_contents.as_bytes()) {
+                IResult::Done(_, ast) => {
+                    let mut map: HashMap<String, Datatype> = HashMap::new();
+                    let program_return_value = ast.evaluate(&mut map);
+                    println!("{:?}", program_return_value);
+                },
+                IResult::Error(e) => eprintln!("encountered an error while parsing the file: {:?}", e),
+                IResult::Incomplete(i) => eprintln!("Couldn't parse all of the file: {:?}", i)
+            }
+        }
+        None => repl() // If a file to run wasn't provided, drop the user into a REPL
+    }
 }
 
 

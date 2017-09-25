8c250ef2b465f31990cb2bc13ecab6092ec5ddc4
Henry Zimmerman <zimhen7@gmail.com>
2017-09-25 19:17:45 -0400

    fleshed out the replace_escapes() to replace other escape sequences

--- src/preprocessor/mod.rs
+++ src/preprocessor/mod.rs
@@ -6,19 +6,31 @@ pub fn preprocess(string: &str) -> String {
 }
 
 
-// Todo: this replaces instances of \\n \\t and \\r with only \n. This should replace the relevant escape sequence.
+/// This replaces \\<escape> instances with \<escape>.
+/// The readline functionality sanitizes these escapes with the double backslash, this returns them to the form they were entered with.
+// TODO: This uses 4 regexes to alter 4 different strings, this should be able to be accomplished in a single pass
 fn replace_escapes(string: &str) -> String {
-    let mut edit = Cow::from("");
-    {
-        let newline_re = Regex::new(r"\\(?P<s>[ntr])").unwrap();
-        edit = newline_re.replace_all(string, "\n");
-    }
-    edit.to_string()
+    let newline_re = Regex::new(r"\\n").unwrap();
+    let newline_string = newline_re.replace_all(string, "\n").to_string();
+
+    let tab_re = Regex::new(r"\\t").unwrap();
+    let tab_string = tab_re.replace_all(newline_string.as_str(), "\t").to_string();
+
+    let return_re = Regex::new(r"\\r").unwrap();
+    let return_string = return_re.replace_all(tab_string.as_str(), "\r").to_string();
+
+    let backslash_re = Regex::new(r"\\\\").unwrap();
+    let backslash_string = backslash_re.replace_all(return_string.as_str(), "\\").to_string();
+
+    backslash_string
 }
 
 #[test]
 fn escape_escapes() {
     assert_eq!("\n".to_string(), replace_escapes("\\n"));
     assert_eq!("hello\n".to_string(), replace_escapes("hello\\n"));
-    assert_eq!("hello\nworld", replace_escapes("hello\\nworld") )
+    assert_eq!("hello\nworld", replace_escapes("hello\\nworld"));
+    assert_eq!("hello\tworld", replace_escapes("hello\\tworld"));
+    assert_eq!("hello\n\rworld", replace_escapes("hello\\n\\rworld"));
+    assert_eq!("hello\\world", replace_escapes("hello\\\\world"));
 }

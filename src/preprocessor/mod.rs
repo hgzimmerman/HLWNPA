6d214ba889e95bceaf361a61dce2a37e2c746082
Henry Zimmerman <zimhen7@gmail.com>
2017-09-25 13:37:17 -0400

    Added todo

--- src/preprocessor/mod.rs
+++ src/preprocessor/mod.rs
@@ -1,18 +1,24 @@
 use regex::Regex;
+use std::borrow::Cow;
 
 pub fn preprocess(string: &str) -> String {
     replace_escapes(string)
 }
 
+
+// Todo: this replaces instances of \\n \\t and \\r with only \n. This should replace the relevant escape sequence.
 fn replace_escapes(string: &str) -> String {
-    let newline_re = Regex::new(r"\\n").unwrap();
-    newline_re.replace_all(string, "\n").to_string()
+    let mut edit = Cow::from("");
+    {
+        let newline_re = Regex::new(r"\\(?P<s>[ntr])").unwrap();
+        edit = newline_re.replace_all(string, "\n");
+    }
+    edit.to_string()
 }
 
 #[test]
 fn escape_escapes() {
     assert_eq!("\n".to_string(), replace_escapes("\\n"));
     assert_eq!("hello\n".to_string(), replace_escapes("hello\\n"));
-    let mut to_edit: String = "hello\\nworld".to_string();
     assert_eq!("hello\nworld", replace_escapes("hello\\nworld") )
 }

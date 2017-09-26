56c44a9de928c1daf2451d926cac1daee0873bf9
Henry Zimmerman <zimhen7@gmail.com>
2017-09-25 20:46:36 -0400

    added bench for preprocessor

--- src/preprocessor/mod.rs
+++ src/preprocessor/mod.rs
@@ -15,13 +15,25 @@ fn replace_escapes(string: &str) -> String {
         .replace(r#"\""#, "\"")
 }
 
-#[test]
-fn escape_escapes() {
-    assert_eq!("\n".to_string(), replace_escapes("\\n"));
-    assert_eq!("hello\n".to_string(), replace_escapes("hello\\n"));
-    assert_eq!("hello\nworld", replace_escapes("hello\\nworld"));
-    assert_eq!("hello\tworld", replace_escapes("hello\\tworld"));
-    assert_eq!("hello\n\rworld", replace_escapes("hello\\n\\rworld"));
-    assert_eq!("hello\\world", replace_escapes("hello\\\\world"));
-    assert_eq!("hello\"world", replace_escapes(r#"hello\\"world"#));
+#[cfg(test)]
+mod test {
+    use super::*;
+    use testing::test_constants;
+    use test::Bencher;
+
+    #[test]
+    fn escape_escapes() {
+        assert_eq!("\n".to_string(), replace_escapes("\\n"));
+        assert_eq!("hello\n".to_string(), replace_escapes("hello\\n"));
+        assert_eq!("hello\nworld", replace_escapes("hello\\nworld"));
+        assert_eq!("hello\tworld", replace_escapes("hello\\tworld"));
+        assert_eq!("hello\n\rworld", replace_escapes("hello\\n\\rworld"));
+        assert_eq!("hello\\world", replace_escapes("hello\\\\world"));
+        assert_eq!("hello\"world", replace_escapes(r#"hello\\"world"#));
+    }
+
+    #[bench]
+    fn preprocess_simple_program_bench(b: &mut Bencher) {
+        b.iter(|| preprocess(test_constants::SIMPLE_PROGRAM_INPUT_1));
+    }
 }

881528ccc97816a4e24906ca78e0eebd15a665c6
Henry Zimmerman <zimhen7@gmail.com>
2017-10-02 11:17:19 -0400

    cleanup of no_keyword_token_group

--- src/parser/utilities.rs
+++ src/parser/utilities.rs
@@ -4,10 +4,7 @@ use ast::Ast;
 
 use parser::literal::literal;
 use parser::identifier::identifier;
-//use parser::assignment::assignment;
 use parser::structure::create_struct_instance;
-//use parser::function::function_execution;
-//use parser::control_flow::control_flow;
 use parser::expressions::{sexpr_parens, unary_operator_and_operand};
 
 
@@ -17,11 +14,11 @@ use parser::expressions::{sexpr_parens, unary_operator_and_operand};
 /// This is used in the sexpr parser, as anything that could parse an expression could blow up the
 /// stack, and that parser isn't interested in evaluating assignments, function definitions, etc...
 named!(pub no_keyword_token_group <Ast>,
-    alt!(
-        complete!(literal) |
-        complete!(identifier) |
-        complete!(create_struct_instance) |
-        complete!(sexpr_parens) |
-        complete!(unary_operator_and_operand)
+    alt_complete!(
+        literal |
+        identifier |
+        create_struct_instance |
+        sexpr_parens |
+        unary_operator_and_operand // allows the s-expr to grap negated parameters
     )
 );

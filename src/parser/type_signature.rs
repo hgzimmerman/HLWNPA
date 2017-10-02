bb133a0933db983a1ecae631f597f5df9fdd77ed
Henry Zimmerman <zimhen7@gmail.com>
2017-10-02 11:30:11 -0400

    add array type signature, this should allow arrays to be passed into and returned from functions

--- src/parser/type_signature.rs
+++ src/parser/type_signature.rs
@@ -5,7 +5,7 @@ use datatype::TypeInfo;
 
 /// _ts indicates that the parser combinator is a getting a type signature
 named!(pub type_signature<Ast>,
-   ws!(alt!(number_ts | string_ts | bool_ts ))
+   ws!(alt!(number_ts | string_ts | bool_ts | array_ts ))
 );
 
 named!(number_ts<Ast>,
@@ -26,3 +26,23 @@ named!(bool_ts<Ast>,
         tag!("Bool")
     )
 );
+
+named!(array_ts<Ast>,
+    do_parse!(
+        contained_type: delimited!(
+            char!('['),
+            type_signature, // TODO find a way to support custom types
+            char!(']')
+        ) >>
+        (Ast::Type(TypeInfo::Array(Box::new( get_type_from_ast(contained_type)) )))
+    )
+);
+
+/// From an AST extract the type info.
+/// Can panic.
+fn get_type_from_ast(ast: Ast) -> TypeInfo {
+    match ast {
+        Ast::Type(info) => info,
+        _ => panic!("Tried to get type from non-type")
+    }
+}

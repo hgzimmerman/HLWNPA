0ab53afec9dc6c7648838f06d68512f3f9544745
Henry Zimmerman <zimhen7@gmail.com>
2017-10-02 12:01:08 -0400

    added test to verify that a function can return an array

--- src/parser/function.rs
+++ src/parser/function.rs
@@ -118,4 +118,34 @@ mod test {
         });
         assert_eq!(expected_fn, value)
     }
+
+   #[test]
+    fn parse_whole_function_identifier_input_returns_array_test() {
+        let input_string = "fn test_function ( a : CustomType ) -> [Number] { [0] }";
+        let (_, value) = match function(input_string.as_bytes()) {
+            IResult::Done(rest, v) => (rest, v),
+            IResult::Error(e) => panic!("{}", e),
+            _ => panic!(),
+        };
+
+        let expected_fn: Ast = Ast::SExpr(SExpression::CreateFunction {
+            identifier: Box::new(Ast::ValueIdentifier("test_function".to_string())),
+            function_datatype: Box::new(Ast::Literal(Datatype::Function {
+                parameters: Box::new(Ast::ExpressionList(vec![
+                    Ast::SExpr(SExpression::TypeAssignment {
+                        identifier: Box::new(Ast::ValueIdentifier("a".to_string())),
+                        type_info: Box::new(Ast::ValueIdentifier("CustomType".to_string()))
+                    })
+                ])),
+                body: Box::new(Ast::ExpressionList(vec![
+                    Ast::Literal(Datatype::Array{
+                        value: vec!(Datatype::Number(0)),
+                        type_: TypeInfo::Number
+                    })
+                ])),
+                return_type: Box::new(Ast::Type(TypeInfo::Array(Box::new(TypeInfo::Number)))),
+            })),
+        });
+        assert_eq!(expected_fn, value)
+    }
 }
